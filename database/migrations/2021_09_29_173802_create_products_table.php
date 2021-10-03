d8fc006fe198d96e63a066dfd2f73e2b0ae91d9f
Omar Attya Pero <58884931+omar95-pero@users.noreply.github.com>
2021-10-03 22:06:46 +0200

    Dtails Component

---
+++ database/migrations/2021_09_29_173802_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('short_description')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('sale_price');
+            $table->string('SKU');
+            $table->enum('stock_status',['in_stock','out_of_stock']);
+            $table->boolean('featured')->default(false);
+            $table->unsignedBigInteger('quantity')->default(0);
+            $table->text('image')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
